b144b58ff2ff8093fe625b9abdf5dee3102cd703
CHEONGBOOHENG <87118516+CheongBooHeng02@users.noreply.github.com>
2023-07-06 15:34:20 +0800

    done homework

--- project/nav.php
+++ project/nav.php
@@ -1,6 +1,6 @@
 <nav class="nav justify-content-end bg-info p-2 rounded-bottom">   
-    <a class="nav-link text-dark" href="#">Home</a>
+    <a class="nav-link text-dark" href="home.php">Home</a>
     <a class="nav-link text-dark" href="product_create.php">Create Product</a>
-    <a class="nav-link text-dark" href="#">Create Customer</a>
+    <a class="nav-link text-dark" href="create_customer.php">Create Customer</a>
     <a class="nav-link text-dark" href="contact_form.php">Contact Us</a>
 </nav>
