2458abec3e7d737d12c2112c58c4fe726bd149ad
CHEONGBOOHENG <87118516+CheongBooHeng02@users.noreply.github.com>
2023-07-27 16:28:38 +0800

    change a link to categories_update.php

--- project/categories_read.php
+++ project/categories_read.php
@@ -81,7 +81,7 @@
                 echo "<a href='customer_read_one.php?id={$id}' class='btn btn-info me-3'>Read</a>";
 
                 // we will use this links on next part of this post
-                echo "<a href='customer_update.php?id={$id}' class='btn btn-primary me-3'>Edit</a>";
+                echo "<a href='categories_update.php?id={$id}' class='btn btn-primary me-3'>Edit</a>";
 
                 // we will use this links on next part of this post
                 echo "<a href='#' onclick='delete_product({$id});'  class='btn btn-danger'>Delete</a>";
