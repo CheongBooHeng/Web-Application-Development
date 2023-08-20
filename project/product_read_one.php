5406d7f52c08329733fc1ebad00bde98071f5dee
CHEONGBOOHENG <87118516+CheongBooHeng02@users.noreply.github.com>
2023-08-20 18:19:42 +0800

    add the coming soon image for products

--- project/product_read_one.php
+++ project/product_read_one.php
@@ -95,7 +95,13 @@
             </tr>
             <tr>
                 <td>Image</td>
-                <td><img src="uploads/<?php echo htmlspecialchars($image, ENT_QUOTES); ?>"></td>
+                <td><?php
+                        if ($image != "") {
+                            echo '<img src="uploads/' . htmlspecialchars($image, ENT_QUOTES) . '">';
+                        } else {
+                            echo '<img src="img/comingsoon.jpg" alt="image">';
+                        }
+                        ?></td>
             </tr>
             <tr>
                 <td></td>
