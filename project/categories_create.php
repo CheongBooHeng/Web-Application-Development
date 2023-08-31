7c0989f3ede3e68d0ca8a7ecebad88865a667c41
CHEONGBOOHENG <87118516+CheongBooHeng02@users.noreply.github.com>
2023-08-31 10:35:37 +0800

    add back button for categories create

--- project/categories_create.php
+++ project/categories_create.php
@@ -85,6 +85,7 @@
                     <td></td>
                     <td>
                         <input type='submit' value='Save' class='btn btn-primary' />
+                        <a href='categories_read.php' class='btn btn-danger'>Back to read categories</a>
                     </td>
                 </tr>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
